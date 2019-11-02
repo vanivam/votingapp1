b167ecd78beaa92352fd3e4783a6811080c65b26
Jason Ge <gejun_1978@yahoo.com>
2019-11-02 08:38:13 +0800

    [DO NOT PUSH] Notes of reading vote app

--- vote/app.py
+++ vote/app.py
@@ -6,17 +6,21 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Cats")
+# gjnote: this is to get the form options
 option_b = os.getenv('OPTION_B', "Dogs")
 option_c = os.getenv('OPTION_C', "Donkeys")
+# gjnote: By default, the hostname inside a container will be the short id of that container
 hostname = socket.gethostname()
 
 app = Flask(__name__)
 
 def get_redis():
     if not hasattr(g, 'redis'):
+        # gjnote: A namespace object that can store data during an application context
         g.redis = Redis(host="redis", db=0, socket_timeout=5)
     return g.redis
 
+# gjnote: Routing that handle both the POST and GET request
 @app.route("/", methods=['POST','GET'])
 def hello():
     voter_id = request.cookies.get('voter_id')
@@ -25,12 +29,22 @@ def hello():
 
     vote = None
 
+    print("Inside the root endpoint of vote")
+
+    # gjnote: when the request is after the user clicks one option
     if request.method == 'POST':
         redis = get_redis()
+        # gjnote: Get vote data from form of the request, and send it to redis
         vote = request.form['vote']
+        print("The vote value is", vote)
+        # gjnote: output is like this: ('The vote value is', u'a')
         data = json.dumps({'voter_id': voter_id, 'vote': vote})
+        # gjnote: output is like this: ('The data value is', '{"vote": "a", "voter_id": "77fbfdf7f8fbfcb6"}')
+
+        print("The data value is", data)
         redis.rpush('votes', data)
 
+    # gjnote: pass option and values to view (index.html). Render it
     resp = make_response(render_template(
         'index.html',
         option_a=option_a,
@@ -44,4 +58,5 @@ def hello():
 
 
 if __name__ == "__main__":
+    # The flask web app runs from here
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
